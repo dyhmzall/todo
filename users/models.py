439a1fa10afe6691b871b887f288f1e79ae77f31
dyhmzall <dyhmzall@gmail.com>
2021-12-24 19:26:40 +0300

    Homework after lesson 1

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from uuid import uuid4
+
+
+class Users(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4())
+    username = models.CharField(max_length=128)
+    firstname = models.CharField(max_length=128)
+    lastname = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
