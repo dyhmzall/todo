e9c9b55e83d1fa4e02dc1075314992a53de18645
dyhmzall <dyhmzall@gmail.com>
2022-01-18 18:02:14 +0300

    Homework after lesson 3

--- project/serializers.py
+++ project/serializers.py
@@ -1,14 +1,20 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
 from .models import Project, Todo
+from users.serializers import UserModelSerializer
 
 
 class ProjectModelSerializer(HyperlinkedModelSerializer):
+    users = UserModelSerializer(many=True)
+
     class Meta:
         model = Project
         fields = '__all__'
 
 
 class TodoModelSerializer(HyperlinkedModelSerializer):
+    user = UserModelSerializer()
+    project = ProjectModelSerializer()
+
     class Meta:
         model = Todo
         fields = '__all__'
